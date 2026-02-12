709c848a7b9e4404dafe96f8333281a478cac142
kimm00 <kimdoyi02@naver.com>
2026-02-12 16:41:40 +0900

    stream

--- java/hello/src/main/java/com/hana8/hello/StreamPlay.java
+++ java/hello/src/main/java/com/hana8/hello/StreamPlay.java
@@ -1,2 +1,84 @@
-package com.hana8.hello;public class StreamPlay {
+package com.hana8.hello;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamPlay {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("JS", "TS", "Java", "JS");
+		Stream<String> stream = list.stream();
+		List<String> list1 = list.stream().toList();
+//		List<String> collect = list.stream().collect(Collectors.toList());
+
+		System.out.println("list.stream().collect(Collections.joining(\", \")) = " + list.stream()
+				.collect(Collectors.joining(", ")));
+		String collect = list.stream().collect(Collectors.joining(", "));
+		String collect2 = String.join(", ", list);
+		System.out.println("collect2 = " + collect2);
+		// :: method 참조
+		list.forEach(System.out::print);
+		list.stream().forEach(System.out::println);
+
+		System.out.println("collect(Collectors.groupingBy(String::length)) = " + list.stream()
+				.collect(Collectors.groupingBy(String::length)));
+
+		// 각 문자열의 길이의 총합
+		// s -> s.length() => String::length
+		long count = list.stream().map(String::length).count();
+		System.out.println("count = " + count);
+//		long count = list.stream().map(String::length).sum(); // sum() 없음.
+
+		Stream<Integer> stream1 = list.stream().map(String::length);
+		IntStream intStream = list.stream().mapToInt(String::length);
+//		IntStream.sum(); // IntStream은 sum() 존재함.
+
+		System.out.println("list.stream().mapToInt(String::length).sum() = " + list.stream()
+				.mapToInt(String::length).sum());
+
+		System.out.println("IntStream.range(1,10) = " + Arrays
+				.toString(IntStream.range(1, 10).toArray()));
+		IntStream intStream1 = IntStream.rangeClosed(1, 10);
+		System.out.println("intStream1.sum() = " + intStream1.sum());
+
+		Map<String, Integer> map1 = new HashSet<>(list).stream()
+				.collect(Collectors.toMap(s -> s, String::length));
+		System.out.println("map1 = " + map1);
+
+		Collections.swap(list, 1, 2);
+		System.out.println("list = " + list);
+
+		/* Default Lambda Functional Interface Examples */
+		// Function은 입력 하나만 받음
+//		Function<String, Integer> length = s -> s.length();
+		Function<String, Integer> length2 = String::length; // 위랑 동일식
+
+		Consumer<String> print = System.out::println;
+
+		System.out.println("length.apply(\"Str\") = " + length2.apply("Str"));
+		print.accept("Jade");
+
+		int[] array = IntStream.range(1, 10).skip(3).limit(5).toArray();
+		System.out.println("array = " + Arrays.toString(array));
+
+		Map<Integer, List<String>> collect1 = list.stream()
+				.collect(Collectors.groupingBy(String::length));
+
+		List<Integer> list2 = list.stream().map(String::length).toList();
+		List<List<Integer>> list3 = list.stream().map(s -> List.of(s.length(), s.length() + 1))
+				.toList();
+		System.out.println("list3 = " + list3);
+		List<Integer> list4 = list.stream().flatMap(s -> Stream.of(s.length(), s.length() + 1))
+				.toList();
+		System.out.println("list4 = " + list4);
+
+	}
 }
--- java/hello/src/main/java/com/hana8/hello/trythis/Stream.java
+++ java/hello/src/main/java/com/hana8/hello/trythis/Stream.java
@@ -1,2 +1,23 @@
-package com.hana8.hello.trythis;public class Stream {
+package com.hana8.hello.trythis;
+
+import java.util.List;
+
+public class Stream {
+
+	public static void main(String[] args) {
+		List<Integer> list = List.of(1, 10, 6, 3, 3, 5, 4, 2, 7, 7, 9, 8, 10);
+
+		System.out.println("짝수의 개수");
+		list.stream().filter(i -> i % 2 == 0).count();
+		System.out.println("각 숫자를 제곱");
+		System.out.println("중복 제거");
+		System.out.println("기본 정렬");
+		System.out.println("역순(내림차순) 정렬");
+		System.out.println("처음 5개만 출력");
+		System.out.println("처음 5개 건너뛰고 출력");
+		System.out.println("값이 5보다 큰 것만 출력");
+		System.out.println("1~10의 합계");
+		System.out.println("random 5개의 평균");
+	}
+
 }
