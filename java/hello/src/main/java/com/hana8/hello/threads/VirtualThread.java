ea0867d4673970abd928b39e496468cc6b7b7abe
kimm00 <kimdoyi02@naver.com>
2026-02-23 10:24:59 +0900

    virtual thread

--- java/hello/src/main/java/com/hana8/hello/threads/VirtualThread.java
+++ java/hello/src/main/java/com/hana8/hello/threads/VirtualThread.java
@@ -1,2 +1,18 @@
-package com.hana8.hello.threads;public class VirtualThread {
+package com.hana8.hello.threads;
+
+public class VirtualThread {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10000; i++) {
+			final int ii = i;
+			Thread.ofVirtual().start(() -> {
+				System.out.println("Thread" + ii);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			});
+		}
+	}
 }
